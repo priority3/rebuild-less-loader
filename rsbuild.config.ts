c7966158e1a5a08b487ed0a7d07ab63e857bb2a5
priority3 <prioritycc@outlook.com>
2024-09-25 20:54:28 +0800

    feat: support css module type

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginLess } from '@rsbuild/plugin-less';
+import { pluginTypedCSSModules } from '@rsbuild/plugin-typed-css-modules';
 
 export default defineConfig({
-  plugins: [pluginReact(),pluginLess()],
+  plugins: [pluginReact(),pluginLess(),pluginTypedCSSModules()],
+  output: {
+    cssModules: {
+      exportLocalsConvention: 'camelCase',
+      auto: (name) => !/(global.(le|sc|sa)ss|\.css)$/.test(name),
+      localIdentName: '[local]-[hash:6]',
+    }
+  },
   source: {
     alias: {
       '@animate/': './node_modules/animate.less/animate.less'
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './App.less';
+import styles from './App.less';
 
 const App = () => {
   return (
-    <div className="content">
+    <div className={styles.content}>
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
